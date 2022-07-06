e198ac29db9e624e59a2fc6f2f9084a460096115
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2022-07-06 01:36:26 -0300

    atualização

--- js/action.js
+++ js/action.js
@@ -1,15 +1,14 @@
 function texto() {
-
-
-
-
+    var palavras = document.getElementById("edit_produto");
+    document.getElementById('name_produto').innerHTML = palavras.value;
+    document.getElementById('name_produto_1').innerHTML = palavras.value;
 }
 
 function valor() {
+    var valores = document.getElementById("valor_produto");
 
 
 
-
-
-
+    document.getElementById('nome_produto').innerHTML = valores.value;
+    document.getElementById('valor_produto_1').innerHTML = valores.value;
 }
